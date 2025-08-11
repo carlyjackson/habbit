0ce0d095093787292a4074ec6f17208c8c5b8ca2
carlyjackson <ctjackson5@gmail.com>
2025-08-11 12:37:51 -1000

    update server with chat router and initial graphql setup

--- server/server.ts
+++ server/server.ts
@@ -1,12 +1,28 @@
 import express from 'express';
 import habbitRouter from './routers/habbitRouter';
-const app = express();
-app.use(express.json()); // Import the habbitRouter
+import chatRouter from './routers/chatRouter';
+// import { ApolloServer } from 'apollo-server-express';
+// import { typeDefs, resolvers } from './graphql'; // define these in your project
+// const server = new ApolloServer({ typeDefs, resolvers });
 
 const PORT = process.env.PORT || 3000;
 
+const app = express();
+// await server.start();
+// server.applyMiddleware({ app, path: '/graphql' });
+app.use(express.json()); 
+
+
+
 app.use('/habbits', habbitRouter); 
-// include middleware controller to handle the request to the SQL DB 
+
+app.use('/chat', chatRouter);
+
+
+
+// app.listen(4000, () => {
+//   console.log('Server running at http://localhost:4000/graphql');
+// });
 
 
 app.listen(PORT, () => {
